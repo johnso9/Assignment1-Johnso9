0342b49d017fe2ae18791a4e28d20c7a7bf275d1
johnso9 <146889665+johnso9@users.noreply.github.com>
2025-01-23 22:34:04 -0500

    Initial commit

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -0,0 +1,36 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("** Starting Maze Runner");
+        try {
+            logger.trace("**** Reading the maze from file " + args[0]);
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (int idx = 0; idx < line.length(); idx++) {
+                    if (line.charAt(idx) == '#') {
+                        logger.info("WALL ");
+                    } else if (line.charAt(idx) == ' ') {
+                        logger.info("PASS ");
+                    }
+                }
+                logger.trace(System.lineSeparator());
+            }
+        } catch(Exception e) {
+            logger.error("/!\\ An error has occured /!\\"); 
+        }
+        logger.info("**** Computing path");
+        logger.error("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
+    }
+}
